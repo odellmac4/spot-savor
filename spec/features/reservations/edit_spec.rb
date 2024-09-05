2e7c41bae47bf99f989b17c15eb41864ccc5f054
Odell McFarland IV <odell.mac4@gmail.com>
2024-09-04 23:58:27 -0700

    Test: validate reservation cannot be updated if chosen table already has an existing reservation for preferred time

--- spec/features/reservations/edit_spec.rb
+++ spec/features/reservations/edit_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Reservation update' do
     let(:table_2) {create(:table, capacity: 6)}
 
     let(:reservation_1) {create(:reservation, name: "Reyonce", start_time: DateTime.new(2024, 12, 24, 10, 0, 0), party_count: 7, table_id: table_1.id)}
-    let(:reservation_2) {create(:reservation, party_count: 6, table_id: table_2.id)}
+    let(:reservation_2) {create(:reservation, name: "Riot Johnson", start_time: DateTime.new(2024, 11, 10, 19, 0, 0), party_count: 5, table_id: table_2.id)}
 
     it 'form fields are pre-filled with reservation data' do
       visit edit_reservation_path(reservation_1.id)
@@ -20,6 +20,53 @@ RSpec.describe 'Reservation update' do
         expect(page).to have_select("am-pm", selected: "AM")
         expect(page).to have_select("reservation-table", selected: "Table #{table_1.id} - Capacity: 8")
         expect(page).to have_button("Update Reservation")
+      end
+    end
+
+    it 'throws an error when trying to update reservation table where capacity is smaller than party count' do
+      visit edit_reservation_path(reservation_2.id)
+
+      within ".edit-reservation-form" do
+
+        select "7", from: "reservation-party"
+  
+        click_button("Update Reservation")
+
+        expect(page).to have_content "Party count is too big for the table capacity"
+      end
+    end
+
+    it 'throws an error when trying to update reservation start time to a time that is not at least an hour after current time' do
+      visit edit_reservation_path(reservation_1.id)
+
+      within ".edit-reservation-form" do
+
+        select "July", from: "reservation-month"
+  
+        click_button("Update Reservation")
+
+        expect(page).to have_content "Start time must be at least an hour after the current time"
+      end
+    end
+
+    it 'throws an error when trying to update reservation to an existing date for the chosen table' do
+      reservation = create(:reservation, start_time: DateTime.new(2024, 10, 10, 19, 0, 0), party_count: 5, table_id: table_2.id)
+
+      visit edit_reservation_path(reservation_1.id)
+
+      within ".edit-reservation-form" do
+
+        select "6", from: "reservation-party"
+        select "2024", from: "reservation-year"
+        select "October", from: "reservation-month"
+        select "10", from: "reservation-day"
+        select "07:00", from: "reservation-time"
+        select "PM", from: "am-pm"
+        select "Table #{table_2.id} - Capacity: 6", from: "reservation-table"
+  
+        click_button("Update Reservation")
+
+        expect(page).to have_content "Start time has already been reserved"
       end
     end
   end
