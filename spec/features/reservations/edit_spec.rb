9dccecc3f6c3f20e2c521925cc6c2596f999b3f3
Odell McFarland IV <odell.mac4@gmail.com>
2024-09-06 04:53:52 -0700

    Test: form throws multiple errors if more than one validation is activated

--- spec/features/reservations/edit_spec.rb
+++ spec/features/reservations/edit_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Reservation update' do
+  let!(:table_1) {create(:table, capacity: 8)}
+  let!(:table_2) {create(:table, capacity: 6)}
+
+  let(:reservation_1) {create(:reservation, name: "Reyonce", start_time: DateTime.new(2024, 12, 24, 10, 0, 0), party_count: 7, table_id: table_1.id)}
+  let(:reservation_2) {create(:reservation, name: "Riot Johnson", start_time: DateTime.new(2024, 11, 10, 19, 0, 0), party_count: 5, table_id: table_2.id)}
+
   describe 'form to edit an existing reservation' do
-    let!(:table_1) {create(:table, capacity: 8)}
-    let!(:table_2) {create(:table, capacity: 6)}
-
-    let(:reservation_1) {create(:reservation, name: "Reyonce", start_time: DateTime.new(2024, 12, 24, 10, 0, 0), party_count: 7, table_id: table_1.id)}
-    let(:reservation_2) {create(:reservation, name: "Riot Johnson", start_time: DateTime.new(2024, 11, 10, 19, 0, 0), party_count: 5, table_id: table_2.id)}
-
     it 'form fields are pre-filled with reservation data' do
       visit edit_reservation_path(reservation_1.id)
 
@@ -23,7 +23,7 @@ RSpec.describe 'Reservation update' do
       end
     end
 
-    it 'successfully updates a reservation' do
+    it 'successfully updates a reservation and leads to reservation show page' do
       visit edit_reservation_path(reservation_2.id)
 
       within ".edit-reservation-form" do
@@ -37,6 +37,23 @@ RSpec.describe 'Reservation update' do
         select "Table #{table_1.id} - Capacity: 8", from: "reservation-table"
 
         click_button("Update Reservation")
+      end
+
+      expect(current_path).to eq reservation_path(reservation_2.id)
+      visit reservation_path(reservation_2.id)
+
+      within ".reservation-show-details" do
+        expect(page).to have_content("Party Count: 6")
+        expect(page).to have_content("Reservation Date: Sun, Jan 12 2025")
+        expect(page).to have_content("Reservation Time: 08:00 PM")
+        expect(page).to have_content("Table #{table_1.id}")
+        expect(page).to have_content("Happy")
+
+        expect(page).to_not have_content("Party Count: 5")
+        expect(page).to_not have_content("Reservation Date: Sun, Nov 10 2024")
+        expect(page).to_not have_content("Reservation Time: 07:00 PM")
+        expect(page).to_not have_content("Table #{table_2.id}")
+        expect(page).to_not have_content("Reyonce")
       end
 
       visit reservations_path
@@ -82,6 +99,13 @@ RSpec.describe 'Reservation update' do
         expect(page).to have_content "Start time must be at least an hour after the current time"
       end
 
+      visit reservation_path(reservation_1.id)
+
+      within ".reservation-show-details" do
+        expect(page).to have_content("Reservation Date: Tue, Dec 24 2024")
+        expect(page).to_not have_content("Reservation Date: Wed, Jul 10 2024")
+      end
+
       visit reservations_path
 
       within "#reservation-#{reservation_1.id}" do
@@ -107,6 +131,121 @@ RSpec.describe 'Reservation update' do
         click_button("Update Reservation")
 
         expect(page).to have_content "Start time has already been reserved"
+      end
+
+      visit reservation_path(reservation_1.id)
+
+      within ".reservation-show-details" do
+        expect(page).to have_content('Reyonce')
+        expect(page).to have_content("Party Count: 7")
+        expect(page).to have_content("Reservation Date: Tue, Dec 24 2024")
+        expect(page).to have_content("Reservation Time: 10:00 AM")
+        expect(page).to have_content("Table #{table_1.id}")
+
+        expect(page).to_not have_content("Party Count: 6")
+        expect(page).to_not have_content("Reservation Date: Thu, Oct 10 2024")
+        expect(page).to_not have_content("Reservation Time: 07:00 PM")
+        expect(page).to_not have_content("Table #{table_2.id}")
+      end
+
+      visit reservations_path
+
+      within "#reservation-#{reservation_1.id}" do
+        expect(page).to have_content('Reyonce')
+        expect(page).to have_content("Party Count: 7")
+        expect(page).to have_content("Reservation Date: Tue, Dec 24 2024")
+        expect(page).to have_content("Reservation Time: 10:00 AM")
+        expect(page).to have_content("Table #{table_1.id}")
+
+        expect(page).to_not have_content("Party Count: 6")
+        expect(page).to_not have_content("Reservation Date: Thu, Oct 10 2024")
+        expect(page).to_not have_content("Reservation Time: 07:00 PM")
+        expect(page).to_not have_content("Table #{table_2.id}")
+      end
+    end
+
+    it 'throws multiple errors if party count too big and start time is in the past' do
+      visit edit_reservation_path(reservation_1.id)
+
+      within ".edit-reservation-form" do
+        select "7", from: "reservation-party"
+        select "2024", from: "reservation-year"
+        select "March", from: "reservation-month"
+        select "10", from: "reservation-day"
+        select "07:00", from: "reservation-time"
+        select "PM", from: "am-pm"
+        select "Table #{table_2.id} - Capacity: 6", from: "reservation-table"
+  
+        click_button("Update Reservation")
+
+        expect(page).to have_content "Party count is too big for the table capacity"
+        expect(page).to have_content "Start time must be at least an hour after the current time"
+      end
+
+      visit reservation_path(reservation_1.id)
+
+      within ".reservation-show-details" do
+        expect(page).to have_content('Reyonce')
+        expect(page).to have_content("Party Count: 7")
+        expect(page).to have_content("Reservation Date: Tue, Dec 24 2024")
+        expect(page).to have_content("Reservation Time: 10:00 AM")
+        expect(page).to have_content("Table #{table_1.id}")
+
+        expect(page).to_not have_content("Party Count: 6")
+        expect(page).to_not have_content("Reservation Date: Thu, Oct 10 2024")
+        expect(page).to_not have_content("Reservation Time: 07:00 PM")
+        expect(page).to_not have_content("Table #{table_2.id}")
+      end
+
+      visit reservations_path
+
+      within "#reservation-#{reservation_1.id}" do
+        expect(page).to have_content('Reyonce')
+        expect(page).to have_content("Party Count: 7")
+        expect(page).to have_content("Reservation Date: Tue, Dec 24 2024")
+        expect(page).to have_content("Reservation Time: 10:00 AM")
+        expect(page).to have_content("Table #{table_1.id}")
+
+        expect(page).to_not have_content("Party Count: 6")
+        expect(page).to_not have_content("Reservation Date: Thu, Oct 10 2024")
+        expect(page).to_not have_content("Reservation Time: 07:00 PM")
+        expect(page).to_not have_content("Table #{table_2.id}")
+      end
+    end
+
+    it 'throws multiple errors if party count too big and start time has already been reserved' do
+      reservation = create(:reservation, start_time: DateTime.new(2025, 10, 10, 19, 0, 0), party_count: 5, table_id: table_2.id)
+
+      visit edit_reservation_path(reservation_1.id)
+
+      within ".edit-reservation-form" do
+        select "7", from: "reservation-party"
+        select "2025", from: "reservation-year"
+        select "October", from: "reservation-month"
+        select "10", from: "reservation-day"
+        select "07:00", from: "reservation-time"
+        select "PM", from: "am-pm"
+        select "Table #{table_2.id} - Capacity: 6", from: "reservation-table"
+  
+        click_button("Update Reservation")
+
+        expect(page).to have_content "Party count is too big for the table capacity"
+        expect(page).to have_content "Start time has already been reserved"
+      end
+
+      visit reservation_path(reservation_1.id)
+
+      within ".reservation-show-details" do
+        expect(page).to have_content('Reyonce')
+        expect(page).to have_content("Party Count: 7")
+        expect(page).to have_content("Reservation Date: Tue, Dec 24 2024")
+        expect(page).to have_content("Reservation Time: 10:00 AM")
+        expect(page).to have_content("Table #{table_1.id}")
+
+        expect(page).to_not have_content("Party Count: 6")
+        expect(page).to_not have_content("Reservation Date: Thu, Oct 10 2024")
+        expect(page).to_not have_content("Reservation Time: 07:00 PM")
+        expect(page).to_not have_content("Table #{table_2.id}")
       end
 
       visit reservations_path
